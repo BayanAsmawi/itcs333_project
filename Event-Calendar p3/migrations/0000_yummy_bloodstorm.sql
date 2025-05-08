194b7b95b85fcc2926502b8ef74c19c2f1210be5
Sara202109880 <Saramansoor016@gmail.com>
2025-05-09 01:24:30 +0300

    Add files via upload

--- Event-Calendar p3/migrations/0000_yummy_bloodstorm.sql
+++ Event-Calendar p3/migrations/0000_yummy_bloodstorm.sql
@@ -1,1 +1,14 @@
-
+CREATE TABLE "events" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"date" text NOT NULL,
+	"location" text NOT NULL,
+	"description" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"username" text NOT NULL,
+	"password" text NOT NULL,
+	CONSTRAINT "users_username_unique" UNIQUE("username")
+);
